25ec917233bf03d4e021b3fbec6e41e59c0a6b27
mqp99 <m99excellent@gmail.com>
2020-10-01 23:42:19 +0700

    update

---
+++ precache-manifest.473205f406efe4411e1526e1b3eda1e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a817ec882c1688b7a33aaa7db7dbe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6e530ea299541c8e78b",
+    "url": "/static/css/main.c9ece673.chunk.css"
+  },
+  {
+    "revision": "cf916169ec6c4aff93fb",
+    "url": "/static/js/2.e362782b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e362782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e530ea299541c8e78b",
+    "url": "/static/js/main.30c5f272.chunk.js"
+  },
+  {
+    "revision": "d096bbbb8af5e06ca4d6",
+    "url": "/static/js/runtime-main.7acab9f2.js"
+  }
+]);

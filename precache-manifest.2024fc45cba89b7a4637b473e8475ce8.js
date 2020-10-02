3e25541d7c58f30672f04b81cdaba8fe69e6c136
mqp99 <m99excellent@gmail.com>
2020-10-02 12:43:36 +0700

    u

---
+++ precache-manifest.2024fc45cba89b7a4637b473e8475ce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51356b46886bc835ad52f0c1397e344c",
+    "url": "/mlibstarted/index.html"
+  },
+  {
+    "revision": "c39eb1357476237cd2e1",
+    "url": "/mlibstarted/static/css/main.c9ece673.chunk.css"
+  },
+  {
+    "revision": "5f2d8456a26610110482",
+    "url": "/mlibstarted/static/js/2.e252f741.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mlibstarted/static/js/2.e252f741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39eb1357476237cd2e1",
+    "url": "/mlibstarted/static/js/main.b5d31f57.chunk.js"
+  },
+  {
+    "revision": "41dfb5b1b7db74496cfb",
+    "url": "/mlibstarted/static/js/runtime-main.fbd4deb2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.473205f406efe4411e1526e1b3eda1e3.js"
+  "/mlibstarted/precache-manifest.2024fc45cba89b7a4637b473e8475ce8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mlibstarted/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
